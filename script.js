078568508b1bb88bb8f25ba364d110930d0ddd25
Punnawit2015 <punnawit.tak2015@gmail.com>
2025-06-14 16:18:19 +0700

    Sci Pro 14/06/2025

---
+++ script.js
@@ -0,0 +1,7 @@
+// ส่วนนี้คุณสามารถเพิ่ม JavaScript เพิ่มเติมได้ เช่น ปุ่มสลับภาษา หรือการโต้ตอบอื่น ๆ
+function toggleDetail(id) {
+  const el = document.getElementById(id);
+  el.classList.toggle('hidden');
+}
+
+console.log("Cellular Respiration Web Loaded");
